6b2cb0cd7767aa7e9c6ef50604c240f6fc6480f2
enochpark89 <44038883+enochpark89@users.noreply.github.com>
2021-07-13 21:51:47 -0700

    20210731 - Created to-do-list inputs and delete buttons
    When the user enters the to-do-list, it creates a list. They have an option to delete using the delete button next to  list texts.

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-alert("hi");
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+// find the first input inside the toDoForm.
+const toDoInput = toDoForm.querySelector("input");
+
+function deleteToDo(event) {
+   // get the parentNode of a button and delete.
+   const li = event.target.parentElement;
+   li.remove();
+}
+
+function paintToDo(newTodo) {
+   // create a new HTML element (li and span) with the text parameter newTodo.
+   const li = document.createElement("li");
+   const span = document.createElement("span");
+   span.innerText = newTodo;
+   const deleteButton = document.createElement("button");
+   deleteButton.innerText = "❌";
+   deleteButton.addEventListener("click", deleteToDo);
+   li.appendChild(span);
+   li.appendChild(deleteButton);
+
+   // append to the toDoList
+   toDoList.appendChild(li);
+}
+
+function handletoDoSubmit(event) {
+   // enter does not refresh
+   event.preventDefault();
+
+   // TEST: success in return input value: console.log(toDoInput.value);
+
+   // Assign the input value to a constant newTodo and delete the input value.
+   const newTodo = toDoInput.value;
+   toDoInput.value = "";
+
+   paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handletoDoSubmit);
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-#
+# To-Do-List
+
+-  Purpose: to practices VanillaJS by making a To-Do-List app.
+
+# Abstract
+
+-  When the user enters the to-do-list, it saves the list in the local memory until the user erases themselves by clicking a delete button.
+
+# Development Process
+
+1.
